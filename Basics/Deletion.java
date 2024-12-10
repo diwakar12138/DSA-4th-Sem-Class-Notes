f121147fdad059461045f8f9d2730e0c2ee7e29b
Diwakar Gautam <diwakargautam823@gmail.com>
2024-12-10 10:31:15 +0530

    updated 4th lecture

--- Basics/Deletion.java
+++ Basics/Deletion.java
@@ -1,13 +1,23 @@
 package Basics;
 
-public class Deletion {
-    public static void main(String[] args){
-        int arr[]={1,2,3,4,7,6};
-        int element =7;
-        int index=4;
+import java.util.Arrays;
 
+public class Deletion{
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5,6,7};
+        System.out.println("Original Array is:");
+        System.out.println(Arrays.toString(arr));
+        int index=3;
         int n=arr.length;
 
-        int newArr[] = new int[n+1];
+        int newArr[]=new int[n-1];
+        for(int i=0;i<index;i++){
+            newArr[i]=arr[i];
+        }
+        for(int j=index;j<n-1;j++){
+            newArr[j]=arr[j+1];
+        }
+        System.out.println("Array after deletion is:");
+        System.out.println(Arrays.toString(newArr));
     }
 }
